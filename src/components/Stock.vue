ad98bd799504fdbd59ae5bdaf4381e2e0380c860
Ahmad Kayyali <kayyali18@gmail.com>
2019-02-10 16:19:58 -0500

    Two way binding to stock inputs, succesfully grab data from user

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -2,19 +2,49 @@
   <div class="col-sm-6 col-md-4">
     <div class="panel panel-success">
       <div class="panel-heading">
-        <h3 class="panel-title ">NAME <small>(Price: PRICE)</small></h3>
+        <h3 class="panel-title ">
+          {{ stock.name }} <small>(Price: {{ stock.price }})</small>
+        </h3>
       </div>
-      <div class="panel-body">
+      <form class="panel-body" @submit.prevent="buyStock">
         <div class="pull-left">
-          <input type="text" class="form-control" placeholder="Quantity" />
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Quantity"
+            v-model="quantity"
+          />
         </div>
         <div class="pull-right">
           <button class="btn btn-success">Buy</button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
 
 <style lang="sass" scoped>
 
