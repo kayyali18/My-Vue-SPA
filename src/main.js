d0bc85dd4b8e5f4428499c3c14e5d23f5f8d9f97
Ahmad Kayyali <kayyali18@gmail.com>
2019-02-10 23:07:17 -0500

    Firebase fail used localStorage for data persisitence

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
+import VueResource from "vue-resource";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+
+// Connect to firebase
+Vue.http.options.root = "https://vuejs-stock-app-b633d.firebaseio.com/";
 
 Vue.filter("currency", value => "$" + value.toLocaleString());
 
